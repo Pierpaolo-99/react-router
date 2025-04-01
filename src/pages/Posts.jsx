e66ab455df1427a86a9488291048bd7a5bdfe38e
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-04-01 18:45:08 +0200

    fetch posts data from API and update Posts component to use dynamic content

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,25 +1,26 @@
+import { useState, useEffect } from "react";
+
+
 export default function Posts() {
-    // Simulazione di una lista di post
-    const posts = [
-        {
-            id: 1,
-            title: "Come iniziare con React",
-            description: "Una guida introduttiva per imparare React passo dopo passo.",
-            image: "https://via.placeholder.com/300x200",
-        },
-        {
-            id: 2,
-            title: "10 consigli per migliorare il tuo JavaScript",
-            description: "Scopri come scrivere codice JavaScript più pulito ed efficiente.",
-            image: "https://via.placeholder.com/300x200",
-        },
-        {
-            id: 3,
-            title: "Le migliori librerie CSS per il 2025",
-            description: "Una panoramica delle librerie CSS più popolari e utili.",
-            image: "https://via.placeholder.com/300x200",
-        },
-    ];
+
+    const [posts, setPosts] = useState([])
+
+    const base_api_url = 'http://localhost:3006'
+    const posts_endpoint = '/api/v1/posts'
+    const img_endpoint = '/img/posts/'
+
+    function fetchData(url) {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setPosts(data)
+            })
+    }
+
+    useEffect(() => {
+        fetchData(base_api_url + posts_endpoint)
+    }, [])
 
     return (
         <section className="py-5">
@@ -27,13 +28,13 @@ export default function Posts() {
                 <h1 className="mb-4">I Nostri Post</h1>
                 <div className="row">
                     {posts.map((post) => (
-                        <div className="col-md-4 mb-4" key={post.id}>
+                        <div className="col-md-4 mb-4" key={post.slug}>
                             <div className="card">
-                                <img src={post.image} className="card-img-top" alt={post.title} />
+                                <img src={base_api_url + img_endpoint + post.image} className="card-img-top" alt={post.title} />
                                 <div className="card-body">
                                     <h5 className="card-title">{post.title}</h5>
                                     <p className="card-text">{post.description}</p>
-                                    <a href={`/posts/${post.id}`} className="btn btn-primary">
+                                    <a href={`/posts/${post.slug}`} className="btn btn-primary">
                                         Leggi di più
                                     </a>
                                 </div>
