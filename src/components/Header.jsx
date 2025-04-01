b8eaf826cf9beeec9969701506ddaedce8ccca0c
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-04-01 18:10:53 +0200

    refactor Header component to use a menu array for dynamic navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,30 @@
 import { NavLink } from "react-router-dom"
 
 export default function Header() {
+
+    const menu = [
+        {
+            id: 1,
+            path: '/',
+            text: 'Home'
+        },
+        {
+            id: 2,
+            path: '/Chisiamo',
+            text: 'Chi siamo'
+        },
+        {
+            id: 3,
+            path: '/Posts',
+            text: 'Posts'
+        },
+        {
+            id: 4,
+            path: '/Contacts',
+            text: 'Contacts'
+        },
+    ]
+
     return (
         <>
             <header>
@@ -22,18 +46,11 @@ export default function Header() {
                         </button>
                         <div class="collapse navbar-collapse" id="collapsibleNavId">
                             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                                <li class="nav-item">
-                                    <NavLink to={'/'} className={'nav-link'}>Home</NavLink>
-                                </li>
-                                <li class="nav-item">
-                                    <NavLink to={'/Chisiamo'} className={'nav-link'}>Chi siamo</NavLink>
-                                </li>
-                                <li class="nav-item">
-                                    <NavLink to={'/Posts'} className={'nav-link'}>Posts</NavLink>
-                                </li>
-                                <li class="nav-item">
-                                    <NavLink to={'/Contacts'} className={'nav-link'}>Contacts</NavLink>
-                                </li>
+                                {menu.map(item => (
+                                    <li class="nav-item" key={item.id}>
+                                        <NavLink to={item.path} className={'nav-link'}>{item.text}</NavLink>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
                     </div>
