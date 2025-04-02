213aaef4c5a310c58a485bc8290c458e38724e4f
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-04-02 12:23:25 +0200

    add bootstrap-icons and implement navigation buttons in Post component

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function Post() {
 
     const [post, setPost] = useState(null)
+    const [posts, setPosts] = useState([]);
+
+    const navigate = useNavigate()
 
     const { slug } = useParams()
     console.log(slug);
@@ -12,9 +15,34 @@ export default function Post() {
         fetch(`http://localhost:3006/api/v1/posts/${slug}`)
             .then(res => res.json())
             .then(data => {
-                setPost(data)
-            })
-    }, [])
+                setPost(data);
+            });
+
+        fetch(`http://localhost:3006/api/v1/posts`)
+            .then((res) => res.json())
+            .then((data) => {
+                setPosts(data);
+            });
+    }, [slug])
+
+    const handleNextPost = () => {
+        const currentIndex = posts.findIndex(p => p.slug === slug);
+        if (currentIndex !== -1 && currentIndex < posts.length - 1) {
+
+            const nextSlug = posts[currentIndex + 1].slug;
+            navigate(`/posts/${nextSlug}`);
+        }
+    };
+
+    const handlePreviousPost = () => {
+
+        const currentIndex = posts.findIndex(p => p.slug === slug);
+        if (currentIndex > 0) {
+
+            const previousSlug = posts[currentIndex - 1].slug;
+            navigate(`/posts/${previousSlug}`);
+        }
+    };
 
     if (!post) {
         return <p>Loading...</p>;
@@ -44,6 +72,12 @@ export default function Post() {
                     <p className="card-text">
                         <small className="text-muted">Descrizione: {post.content}</small>
                     </p>
+                    <button className="btn btn-primary me-2" onClick={handlePreviousPost}>
+                        <i className="bi bi-arrow-left"></i>
+                    </button>
+                    <button className="btn btn-primary" onClick={handleNextPost}>
+                        <i className="bi bi-arrow-right"></i>
+                    </button>
                 </div>
             </div>
         </div>
